932870491dfbd147dfeda59c446a316c6beff9e7
whj0401 <emls0401@163.com>
2015-02-06 21:39:29 +0800

    先建好最基础的

---
+++ core/src/com/mygdx/game/Dye.java
@@ -0,0 +1,12 @@
+package com.mygdx.game;
+
+public class Dye {
+	int red;
+	int green;
+	int blue;
+	public Dye(int red, int green, int blue){
+		this.red = red;
+		this.green = green;
+		this.blue = blue;
+	}
+}
---
+++ core/src/com/mygdx/game/DyeCrash.java
@@ -0,0 +1,8 @@
+package com.mygdx.game;
+
+public class DyeCrash {
+	
+	public static Dye crash(Dye d1, Dye d2){
+		return null;
+	}
+}
