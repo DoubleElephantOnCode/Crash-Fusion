95936aadd050629e6e59521035e2373ee7f48f6e
reallyLoveIT <1641711860@qq.com>
2015-02-08 16:30:28 +0800

    添加了第一个碰撞抵消的方法

--- core/src/com/mygdx/game/Dye.java
+++ core/src/com/mygdx/game/Dye.java
@@ -4,9 +4,39 @@ public class Dye {
 	int red;
 	int green;
 	int blue;
+	Red redcolor;
+	Green greencolor;
+	Blue bluecolor;
 	public Dye(int red, int green, int blue){
 		this.red = red;
 		this.green = green;
 		this.blue = blue;
+		redcolor = new Red(red);
+		greencolor = new Green(green);
+		bluecolor = new Blue(blue);
+	}
+	
+	
+	
+	public boolean isZero(){
+		return (redcolor.num == 0)&&(greencolor.num == 0)&&(bluecolor.num == 0);
+	}
+	public Colour findMax(){
+		if(redcolor.num >= greencolor.num &&redcolor.num >= bluecolor.num){
+			return redcolor;
+		}else if(greencolor.num >= redcolor.num && greencolor.num >= bluecolor.num){
+			return bluecolor;
+		}else{
+			return greencolor;
+		}
+	}
+	
+	/*
+	 * 将redcolor greencolr bluecolor 里的数据和red green blue等 同步
+	 */
+	public void sync(){
+		red = redcolor.num;
+		green = greencolor.num;
+		blue = bluecolor.num;
 	}
 }
--- core/src/com/mygdx/game/DyeCrash.java
+++ core/src/com/mygdx/game/DyeCrash.java
@@ -1,8 +1,33 @@
 package com.mygdx.game;
 
+import java.util.ArrayList;
+
 public class DyeCrash {
 	
 	public static Dye crash(Dye d1, Dye d2){
-		return null;
+		return firstcrash(d1,d2);
+	}
+	
+	/**
+	 * 第一种抵消方法
+	 * 每次取两个染料最多的颜色进行抵消 直到一方消失
+	 * 存在的问题 ： 如果两种颜色同样最多 ， 会按rgb的顺序优先取，这样不太公平， 以后可以考虑相同时随机取
+	 * @param d1
+	 * @param d2
+	 * @return
+	 */
+	public static Dye firstcrash(Dye d1, Dye d2){
+		while(!(d1.isZero()||d2.isZero())){
+			Colour c1 = d1.findMax();
+			Colour c2 = d2.findMax();
+			c1.crash(c2);
+		}
+		d1.sync();
+		d2.sync();
+		if(d1.isZero()){
+			return d2;
+		}else{
+			return d1;
+		}
 	}
 }
